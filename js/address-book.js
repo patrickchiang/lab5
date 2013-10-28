f9e0de9e401777061c17ad1bffdafc56f0c01376
pchiang <rukqoa@gmail.com>
2013-10-27 21:42:57 -0700

    Commit and done

--- js/address-book.js
+++ js/address-book.js
@@ -1,28 +1,64 @@
 /* address-book.js
-    this is where you will add your JavaScript to complete Lab 5
-*/
+ this is where you will add your JavaScript to complete Lab 5
+ */
 
+$(function() {
+    sortObjArray(Employees.entries, "last");
+    $(".sort-ui .btn").click(sortBy);
+});
+
+function sortBy(handler) {
+    var sortMethod = $(this).attr("data-sortby");   // get the sort method from button
+    sortObjArray(Employees.entries, sortMethod);    // sort & re-render
+    
+    $(".btn.active").removeClass("active");
+    $(this).addClass("active");
+}
+
+function render(entries) {
+    var template = $(".template");
+    var book = $(".address-book");
+
+    // Clear the address book
+    book.empty();
+
+    // Loop through each entry
+    $(entries).each(function(i, val) {
+        var entry = template.clone();
+
+        // Populate info
+        entry.find(".first").html(val.first);
+        entry.find(".last").html(val.last);
+        entry.find(".title").html(val.title);
+        entry.find(".dept").html(val.dept);
+        entry.find(".pic").attr("src", val.pic);
+        entry.find(".pic").attr("alt", "picture of " + val.first + " " + val.last);
+
+        entry.removeClass("template");
+        book.append(entry);
+    });
+}
 
 /* sortObjArray()
-    sorts an array of objects by a given property name
-    the property values are compared using standard 
-    operators, so this will work for string, numeric,
-    boolean, or date values
+ sorts an array of objects by a given property name
+ the property values are compared using standard
+ operators, so this will work for string, numeric,
+ boolean, or date values
 
-    objArray        array of objects to sort
-    propName        property name to sort by
+ objArray        array of objects to sort
+ propName        property name to sort by
 
-    returns undefined (array is sorted in place)
-*/
+ returns undefined (array is sorted in place)
+ */
 function sortObjArray(objArray, propName) {
     if (!objArray.sort)
         throw new Error('The objArray parameter does not seem to be an array (no sort method)');
 
     //sort the array supplying a custom compare function
-    objArray.sort(function(a,b) {
-        
+    objArray.sort(function(a, b) {
+
         //note: this compares only one property of the objects
-        //see the optional step where you can add support for 
+        //see the optional step where you can add support for
         //a secondary sort key (i.e., sort by another property)
         //if the first property values are equal
         if (a[propName] < b[propName])
@@ -32,5 +68,7 @@ function sortObjArray(objArray, propName) {
         else
             return 1;
     });
+
+    render(Employees.entries);  // render after sort
 } //sortObjArray()
 
